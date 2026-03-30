8c4799b31554229c0f650995965a76e0fefbba46
Uma jaiswal <umaj2631@gmail.com>
2026-03-30 16:45:20 +0530

    Added login logout system with dashboard

--- taskify_app/urls.py
+++ taskify_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home' ),
     path('dashboard/', views.dashboard, name='dashboard' ),
+    path('login/', views.user_login, name='login' ),
+    path('logout/', views.user_logout, name='logout' ),
 ]
 
--- taskify_app/views.py
+++ taskify_app/views.py
@@ -1,25 +1,36 @@
 from django.shortcuts import render , redirect
-from django.contrib.auth import authenticate , login
+from django.contrib.auth import authenticate , login , logout
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request,'home.html')
 
+
+@login_required(login_url='login')
 def dashboard(request):
     return render(request,'dashboard.html')
 
 
 def user_login(request):
-    if request.method == 'post':
-        user_name = request.post.get('user_name')
-        user_password = request.post.get('user_password')
+    if request.method == 'POST':
+        user_name = request.POST.get('user_name')
+        user_password = request.POST.get('user_password')
 
-        user = authenticate(request, user_name=user_name, user_password=user_password)
+        user = authenticate(request, username=user_name, password=user_password)
         
         if user is not None :
             login(request, user)
             return redirect('dashboard')
         else :
-            return render(request,'login.html',{'error':'invelid authentication'})
+            return render(request,'login.html',{'error':'invalid authentication'})
     return render(request,  'login.html')
         
-        
+
+
+
+def user_logout(request):
+    logout(request) 
+    return redirect('home')
+
+
+
